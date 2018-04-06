1323433466fb979e9935c0ca9aef92170ffd17ef
Brodey Newman <brodeynewman@gmail.com>
2018-04-06 13:45:39 -0400

    Adding/hiding heroku server url

--- src/config.js
+++
@@ -1,4 +0,0 @@
-/**
- * Socket URL configuration
- */
-export const SOCKET_CONNECTION = 'http://localhost:8008';
--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-import { SOCKET_CONNECTION } from './config';
+import SOCKET_CONNECTION from './socketConfig';
 
 // Building inital sock connection
 const socket = io.connect(SOCKET_CONNECTION);
