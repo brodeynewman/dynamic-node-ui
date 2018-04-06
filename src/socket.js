71cbbef967be5b5684c9c93f865a698fa57cd233
Brodey Newman <brodeynewman@gmail.com>
2018-04-06 17:24:56 -0400

    adding heroku url

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
 // Building inital sock connection
-const socket = io.connect(process.env.SOCKET_CONNECTION);
+const socket = io.connect('https://whispering-anchorage-42548.herokuapp.com/');
 
 export default socket;
