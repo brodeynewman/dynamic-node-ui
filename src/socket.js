d3ef7f0465b252a29daa99810c55c231628f3475
Brodey Newman <brodeynewman@gmail.com>
2018-04-06 14:14:54 -0400

    removing localhost from socket connection

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
 // Building inital sock connection
-const socket = io.connect(process.env.SOCKET_CONNECTION || 'http://localhost:8008');
+const socket = io.connect(process.env.SOCKET_CONNECTION);
 
 export default socket;
